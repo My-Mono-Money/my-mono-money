a66b1f14dfa083b1130dd4c43768261479bae593
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-10 21:45:53 +0300

    MMM-13 setup cd frontend staging

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, reusable workflow (try 4)!
+          Hello, reusable workflow!
         </p>
         <a
           className="App-link"
