e2a7c45746307a73b671e8588582f5df31c5a16e
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-11 11:11:59 +0300

    MMM-14 try 2

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, reusable workflow production!
+          Hello, reusable workflow production (try 2)!
         </p>
         <a
           className="App-link"
