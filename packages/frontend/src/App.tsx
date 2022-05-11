d50ce85f38b415c85f69b3f9f89a4266e6525e8e
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-11 19:30:54 +0300

    MMM-17 config liter frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, reusable workflow production!
-        </p>
+        <p>Hello, reusable workflow production!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
