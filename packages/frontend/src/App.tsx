8e40e453f0430f9855911b319286844a86900847
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-11 20:37:54 +0300

    MMM-20 try 4

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, try 3</p>
+        <p>Hello, try 4</p>
         <a
           className="App-link"
           href="https://reactjs.org"
