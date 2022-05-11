8a4f2c7aea529946951c859349e6e590075a01a0
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-11 20:27:06 +0300

    MMM-20 setup joint verify pr

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, reusable workflow production! (try 3)</p>
+        <p>Hello, try 1</p>
         <a
           className="App-link"
           href="https://reactjs.org"
