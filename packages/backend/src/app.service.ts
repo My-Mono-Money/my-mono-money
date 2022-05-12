9f617259772d514c579fc8700892ad5dc46362c9
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-12 20:27:44 +0300

    MMM-21 try 4

--- packages/backend/src/app.service.ts
+++ packages/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello development! (try 3)';
+    return 'Hello development! (try 4)';
   }
 }
