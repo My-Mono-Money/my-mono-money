b67d061e07d9267fef7c0ba5d9037fc159af17f1
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-13 11:42:07 +0300

    MMM-23 clean up

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello staging (try 1)!</p>
+        <p>Hello staging!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
