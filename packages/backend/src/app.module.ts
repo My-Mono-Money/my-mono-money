051ea708d7654c45008c64ddf970ed9db266fa86
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-17 18:28:46 +0300

    MMM-29 Added and configured typeorm

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [typeOrmConfig],
+      envFilePath: ['.env.local'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get('typeorm');
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
