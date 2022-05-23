e6c4f9df0d9181d3f1446817ee19922884622bea
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-23 17:32:58 +0300

    MMM-35 added setup versioning

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupVersioning } from './common/api/setup-versioning';
 import { setupValidation } from './common/pipes/setup-validation';
 import { setupSwagger } from './docs/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupVersioning(app);
   setupValidation(app);
   setupSwagger(app);
 
