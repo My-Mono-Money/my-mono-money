81a28752ab63865cb3427defecb08f814a528f4a
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-24 18:31:38 +0300

    TEST try 1

--- packages/backend/src/app.service.ts
+++ packages/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello production (test actions!)!';
+    return 'Hello production (test actions! try 1)!';
   }
 }
