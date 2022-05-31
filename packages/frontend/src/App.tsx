e7dee31dee570ce21839649198e9e973d478e357
Anastasiia Salenko <ao.salenko@gmail.com>
2022-05-31 17:51:48 +0300

    MMM-52 Connected MUI to project

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 import logo from './logo.svg';
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Typography>Hello MUI</Typography>
+        <AccessAlarm />
+        <ThreeDRotation />
         <p>Hello production (test actions! try 1)!</p>
         <a
           className="App-link"
