fcc10208740a9a68ef00cb5798c6b6dc327157b5
Anastasiia Salenko <ao.salenko@gmail.com>
2022-06-02 11:31:19 +0300

    MMM-55 implemented auth layout

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AuthLayout from './layouts/auth.layout';
 
 const SignInPage = React.lazy(() => import('./pages/sign-in/sign-in.page'));
 const SignUpPage = React.lazy(() => import('./pages/sign-up/sign-up.page'));
@@ -27,9 +28,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <Suspense fallback={<div>Завантаження...</div>}>
           <Routes>
-            <Route index element={<Navigate to="sign-in" replace />} />
-            <Route path="sign-in" element={<SignInPage />} />
-            <Route path="sign-up" element={<SignUpPage />} />
+            <Route element={<AuthLayout />}>
+              <Route index element={<Navigate to="sign-in" replace />} />
+              <Route path="sign-in" element={<SignInPage />} />
+              <Route path="sign-up" element={<SignUpPage />} />
+            </Route>
           </Routes>
         </Suspense>
       </ThemeProvider>
--- packages/frontend/src/pages/sign-up/sign-up.page.tsx
+++ packages/frontend/src/pages/sign-up/sign-up.page.tsx
@@ -1,42 +1,10 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import AuthLayout from '../../layouts/auth.layout';
 
-const LeftSide = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      pt: '27px',
-      px: '70px',
-      color: 'whitesmoke',
-      background:
-        'linear-gradient(164deg, rgba(97,123,227,1) 0%, rgba(97,121,227,1) 15%, rgba(111,37,220,1) 70%, rgba(112,28,219,1) 100%)',
-    }}
-  >
-    <Typography variant="h6" letterSpacing={3}>
-      My Mono Money
-    </Typography>
-
-    <Box
-      sx={{
-        pt: '100px',
-      }}
-    >
-      <Typography variant="h3" fontWeight={600}>
-        Найкращий застосунок, для ведення домашньої бухгалтерії
-      </Typography>
-    </Box>
-  </Box>
-);
-
-const RightSide = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      pt: 7.5,
-    }}
-  >
+const SignUp: React.FC = () => {
+  return (
     <Box
       sx={{
         display: 'flex',
@@ -75,24 +43,6 @@ const RightSide = () => (
         </Typography>
       </Box>
     </Box>
-  </Box>
-);
-
-const SignUp: React.FC = () => {
-  return (
-    <>
-      <Box
-        sx={{
-          display: 'grid',
-          gridAutoFlow: 'column',
-          gridAutoColumns: '1fr',
-          height: '100%',
-        }}
-      >
-        <LeftSide />
-        <RightSide />
-      </Box>
-    </>
   );
 };
 
