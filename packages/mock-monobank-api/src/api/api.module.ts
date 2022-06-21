e0cc1858d1ff313cbe9e4fcd9c44fd5913d6fd84
Anastasiia Salenko <ao.salenko@gmail.com>
2022-06-21 12:33:07 +0300

    MMM-88 Added dummy statement endpoint

--- packages/mock-monobank-api/src/api/api.module.ts
+++ packages/mock-monobank-api/src/api/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ClientInfoController } from './client-info/client-info.controllers';
+import { StatementController } from './statement/statement.controller';
 
 @Module({
-  controllers: [ClientInfoController],
+  controllers: [ClientInfoController, StatementController],
 })
 export class ApiModule {}
