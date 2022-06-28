7c93e59e9848bdd85f7f5a74b612e1c642bdef52
Anastasiia Salenko <ao.salenko@gmail.com>
2022-06-28 17:10:47 +0300

    MMM-78 clean up

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { setupVersioning } from './common/api/setup-versioning';
 import { setupValidation } from './common/pipes/setup-validation';
 import { setupDocs } from './docs/setup-docs';
-// try 3
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupVersioning(app);
