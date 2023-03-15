82e871d11cbe7d418e9ac3bf9da742bf2f899f35
Anastasiia Salenko <ao.salenko@gmail.com>
2023-03-15 16:03:27 +0200

    MMM 125 Fixed style statement table

--- packages/frontend/src/pages/statements/statements-table.component.tsx
+++ packages/frontend/src/pages/statements/statements-table.component.tsx
@@ -20,6 +20,7 @@ import { UpdatingIndicator } from '../../common/components/updating-indicator/up
 import { useDebounce } from 'use-debounce';
 import PeriodFilter from './period-filter.component';
 import { useSearchParams } from 'react-router-dom';
+import { fromUnixTime, format } from 'date-fns';
 
 interface IStatementsResponse {
   items: IStatementItem[];
@@ -41,6 +42,27 @@ const fetchStatements = async (token: string, page: number, period: string) => {
   } catch (err) {
     console.log('err', err);
   }
+};
+
+const formatAmountCurrency = (num: number) => {
+  const hasMinus = num < 0;
+  const numStr = String(Math.trunc(Math.abs(num) / 100));
+  let result = '';
+  let count = 0;
+
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    result = numStr.charAt(i) + result;
+    count = count + 1;
+    if (count === 3 && i > 0) {
+      result = ' ' + result;
+      count = 0;
+    }
+  }
+  return [hasMinus ? '-' : '', result].join('');
+};
+
+const formatAmountCents = (num: number) => {
+  return '.' + String(Math.abs(num) % 100).padStart(2, '0');
 };
 
 const renderLoadingSkeleton = () => {
@@ -96,12 +118,12 @@ const StatementTable: React.FC = () => {
       {debouncedIsUpdating && <UpdatingIndicator />}
       <Box
         sx={{
-          width: '100%',
           margin: '0 auto',
           borderRadius: '16px',
           border: '1px solid',
           borderColor: 'lightgray',
           boxShadow: '3px 3px 3px 3px lightgray',
+          px: '40px',
         }}
       >
         <Box sx={{ p: 3, display: 'flex' }}>
@@ -115,9 +137,13 @@ const StatementTable: React.FC = () => {
           >
             <TableHead>
               <TableRow>
-                <TableCell>Дата та час</TableCell>
-                <TableCell>Деталі транзкції</TableCell>
-                <TableCell>Сума</TableCell>
+                <TableCell sx={{ width: '115px' }} align="left">
+                  Дата та час
+                </TableCell>
+                <TableCell align="center">Деталі транзкції</TableCell>
+                <TableCell sx={{ width: '115px' }} align="right">
+                  Сума
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody
@@ -128,13 +154,24 @@ const StatementTable: React.FC = () => {
             >
               {debouncedIsLoading && renderLoadingSkeleton()}
               {!debouncedIsLoading &&
-                response?.items.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell>{row.time}</TableCell>
-                    <TableCell>{row.description}</TableCell>
-                    <TableCell>{row.amount}</TableCell>
-                  </TableRow>
-                ))}
+                response?.items.map((row) => {
+                  const formatTime = `${format(
+                    fromUnixTime(row.time),
+                    'dd.MM.yyyy HH:mm',
+                  )}`;
+                  return (
+                    <TableRow key={row.id}>
+                      <TableCell align="left">{formatTime}</TableCell>
+                      <TableCell align="center">{row.description}</TableCell>
+                      <TableCell align="right">
+                        <span>{formatAmountCurrency(row.amount)}</span>
+                        <span style={{ fontSize: '0.6rem' }}>
+                          {formatAmountCents(row.amount)}
+                        </span>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
             </TableBody>
           </Table>
         </TableContainer>
