dd2a19841074aaf24b4c5376ce21a339901a5a6a
Anastasiia Salenko <ao.salenko@gmail.com>
2023-04-12 15:36:34 +0300

    MMM 142 Added script for copying env file

--- packages/backend/scripts/deploy.sh
+++ packages/backend/scripts/deploy.sh
@@ -16,6 +16,7 @@ UNARCHIVECMD=$(cat <<UNARCHIVECMD
 cd "my-mono-money/slots/$ENVIRONMENT_SLOT"
 mkdir "$DEPLOYMENT_NAME"
 tar -xzf dist.tar.gz -C "$DEPLOYMENT_NAME"
+cp config/.env.local "$DEPLOYMENT_NAME"
 rm dist.tar.gz
 UNARCHIVECMD
 )
