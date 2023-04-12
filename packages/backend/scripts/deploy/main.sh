1e98a28409fd99d03c1b6ce2bd3a0c21cb585508
Anastasiia Salenko <ao.salenko@gmail.com>
2023-04-12 19:06:07 +0300

    MMM 142 Added setup nginx

---
+++ packages/backend/scripts/deploy/main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AVAILABLE_ENVIRONMENT_SLOTS=("development" "staging" "production")
+ENVIRONMENT_SLOT=$1
+VM_NAME="do-webapps"
+DEPLOYMENT_NAME="deployment-$(date +'%Y-%m-%d-%H-%M-%S')"
+
+if [[ ! " ${AVAILABLE_ENVIRONMENT_SLOTS[*]} " =~ " ${ENVIRONMENT_SLOT} " ]]; then
+  echo "Unknown environment slot: ${ENVIRONMENT_SLOT}, available environment slots are: ${AVAILABLE_ENVIRONMENT_SLOTS[@]}"
+  exit 1
+fi
+
+scp dist.tar.gz "$VM_NAME:~/my-mono-money/slots/$ENVIRONMENT_SLOT"
+
+cat ./scripts/deploy/remote/unarchive.sh
+ssh "$VM_NAME" "ENVIRONMENT_SLOT=$ENVIRONMENT_SLOT DEPLOYMENT_NAME=$DEPLOYMENT_NAME bash -s" < ./scripts/deploy/remote/unarchive.sh
+
+cat ./scripts/deploy/remote/create-nginx-conf.sh
+ssh "$VM_NAME" "ENVIRONMENT_SLOT=$ENVIRONMENT_SLOT DEPLOYMENT_NAME=$DEPLOYMENT_NAME bash -s" < ./scripts/deploy/remote/create-nginx-conf.sh
+
+cat ./scripts/deploy/remote/install-deps.sh
+ssh "$VM_NAME" "ENVIRONMENT_SLOT=$ENVIRONMENT_SLOT DEPLOYMENT_NAME=$DEPLOYMENT_NAME bash -s" < ./scripts/deploy/remote/install-deps.sh
---
+++ packages/backend/scripts/deploy/remote/unarchive.sh
@@ -0,0 +1,5 @@
+cd "my-mono-money/slots/$ENVIRONMENT_SLOT"
+mkdir "$DEPLOYMENT_NAME"
+tar -xzf dist.tar.gz -C "$DEPLOYMENT_NAME"
+cp config/.env.local "$DEPLOYMENT_NAME"
+rm dist.tar.gz
