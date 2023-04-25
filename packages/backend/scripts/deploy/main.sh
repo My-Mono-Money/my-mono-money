948a72bc5b91892f8cbcf975f9459fe3db7ba174
Anton <example@gmail.com>
2023-04-25 14:22:29 +0300

    fix after review

--- packages/backend/scripts/deploy/main.sh
+++ packages/backend/scripts/deploy/main.sh
@@ -5,6 +5,8 @@ ENVIRONMENT_SLOT=$1
 VM_NAME="do-webapps"
 DEPLOYMENT_NAME="deployment-$(date +'%Y-%m-%d-%H-%M-%S')"
 
+set -e
+set -o pipefail
 if [[ ! " ${AVAILABLE_ENVIRONMENT_SLOTS[*]} " =~ " ${ENVIRONMENT_SLOT} " ]]; then
   echo "Unknown environment slot: ${ENVIRONMENT_SLOT}, available environment slots are: ${AVAILABLE_ENVIRONMENT_SLOTS[@]}"
   exit 1
@@ -29,3 +31,7 @@ ssh "$VM_NAME" "ENVIRONMENT_SLOT=$ENVIRONMENT_SLOT DEPLOYMENT_NAME=$DEPLOYMENT_N
 
 cat ./scripts/deploy/remote/archive-old.sh
 ssh "$VM_NAME" "ENVIRONMENT_SLOT=$ENVIRONMENT_SLOT DEPLOYMENT_NAME=$DEPLOYMENT_NAME bash -s" < ./scripts/deploy/remote/archive-old.sh
+
+ssh "$VM_NAME" "ENVIRONMENT_SLOT=$ENVIRONMENT_SLOT DEPLOYMENT_NAME=$DEPLOYMENT_NAME bash -s" < ./scripts/deploy/remote/backup-db.sh
+
+yarn typeorm migration:run
