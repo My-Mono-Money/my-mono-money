ae7d878fb5b60cc8c135531c45cb15039aa60160
Anton <example@gmail.com>
2023-06-16 12:56:08 +0300

    small fix and added loader

--- packages/frontend/src/common/page-loading/page-loading.tsx
+++ packages/frontend/src/common/page-loading/page-loading.tsx
@@ -1,3 +1,16 @@
+import { Box } from '@mui/material';
 import React from 'react';
-
-export const PageLoading = () => <div>Завантаження...</div>;
+import { InfinitySpin } from 'react-loader-spinner';
+export const PageLoading = () => (
+  <Box
+    sx={{
+      width: '100%',
+      height: '90%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <InfinitySpin width="200" color="#4fa94d" />
+  </Box>
+);
--- packages/frontend/src/pages/statements/statements-table.component.tsx
+++ packages/frontend/src/pages/statements/statements-table.component.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 import { useDebounce } from 'use-debounce';
 import { fromUnixTime, format } from 'date-fns';
