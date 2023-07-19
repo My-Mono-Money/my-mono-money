c83edb7efb0d709be9dd643be0759c5d73c25aeb
Anton <example@gmail.com>
2023-07-19 08:31:15 +0300

    MMM-235 Add option to delete token frontend

--- packages/backend/src/layers/functionality/tokens/delete-token.service.ts
+++ packages/backend/src/layers/functionality/tokens/delete-token.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AccessDeniedError } from '~/common/errors/access-denied-error';
+import { ImportAttemptStatusType } from '~/storage/interfaces/create-monobank-token-import-attempt-dto.interface';
 import { ImportAttemptStorage } from '~/storage/services/import-attempt.storage';
 import { SpaceStorage } from '~/storage/services/space.storage';
 import { TokenStorage } from '~/storage/services/token.storage';
@@ -15,17 +16,33 @@ export class DeleteTokenService {
   constructor(
     private userStorage: UserStorage,
     private tokenStorage: TokenStorage,
+    private importAttemptStorage: ImportAttemptStorage,
   ) {}
 
   async delete({ tokenId, email }: IDeleteTokenArgs) {
-    const currentUserSpace = await this.userStorage.getSpaceByEmail(email);
-    const token = await this.tokenStorage.getTokenByTokenId(tokenId);
-    const isOwnSpace = currentUserSpace.id === token.space.id;
+    try {
+      const currentUserSpace = await this.userStorage.getSpaceByEmail(email);
+      const token = await this.tokenStorage.getTokenByTokenId(tokenId);
+      const isOwnSpace = currentUserSpace.id === token.space.id;
 
-    if (!isOwnSpace) {
-      throw new AccessDeniedError();
+      if (!isOwnSpace) {
+        throw new AccessDeniedError();
+      }
+
+      const tryFindImportAttempt = await this.importAttemptStorage.getByTokenId(
+        token.token,
+      );
+
+      const checkInProgressImport = tryFindImportAttempt.find(
+        (item) => item.status === ImportAttemptStatusType.InProgress,
+      );
+      if (checkInProgressImport) {
+        throw new Error('Statement import in progress');
+      }
+
+      return await this.tokenStorage.deleteToken(tokenId);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-
-    return await this.tokenStorage.deleteToken(tokenId);
   }
 }
--- packages/frontend/src/pages/settings/bank-integration/show-token-list.component.tsx
+++ packages/frontend/src/pages/settings/bank-integration/show-token-list.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Table,
@@ -11,18 +11,65 @@ import {
   Theme,
   Chip,
 } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
 import { format } from 'date-fns';
 import {
   ITokenItem,
+  ImportAttemptStatusType,
   LastWebhookValidationStatusType,
 } from 'types/token-item.interface';
 import { useFetchTokenList } from 'api/useFetchTokenList';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { axiosPrivate } from 'api/axios';
+import { AxiosError } from 'axios';
+import { IErrorResponse } from 'types/error-response.interface';
+import { UpdatingIndicator } from 'common/components/updating-indicator/updating-indicator.component';
 
+interface IAlertRemoveToken {
+  tokenId: string;
+}
 const ShowTokenList: React.FC = () => {
   const isMd = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
   const tokenList = useFetchTokenList();
+  const queryClient = useQueryClient();
+  const [openAlertRemoveToken, setOpenAlertRemoveToken] = useState('');
+
+  const handleDeleteToken = (tokeId: string) => {
+    setOpenAlertRemoveToken(tokeId);
+  };
+
+  const { mutate: mutateDeleteInvite, isLoading } = useMutation({
+    mutationFn: ({ tokenId }: IAlertRemoveToken) =>
+      axiosPrivate.delete(`/tokens/${tokenId}/delete`),
+
+    onError: (err) => {
+      const axiosError = err as unknown as AxiosError<IErrorResponse>;
+      alert(axiosError.response?.data.message);
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(['token-list']);
+    },
+  });
+  const handleClose = () => {
+    setOpenAlertRemoveToken('');
+  };
+
+  const handleConfirm = async () => {
+    if (!isLoading) {
+      mutateDeleteInvite({
+        tokenId: openAlertRemoveToken,
+      } as IAlertRemoveToken);
+      setOpenAlertRemoveToken('');
+    }
+  };
   return (
     <Box sx={{ mb: '20px' }}>
+      {isLoading && <UpdatingIndicator />}
       <>
         {!tokenList?.data ||
           (tokenList.data.length < 1 ? (
@@ -166,22 +213,47 @@ const ShowTokenList: React.FC = () => {
                             {formatTime}
                           </TableCell>
                           <TableCell align="center">
-                            <Chip
-                              size={isMd ? 'small' : 'medium'}
+                            <Box
                               sx={{
-                                ...(isMd && {
-                                  height: '15px',
-                                  fontSize: '1.5vw',
-                                }),
+                                display: 'flex',
+                                gap: '1px',
+                                alignItems: 'center',
                               }}
-                              label={row.lastWebhookValidationStatus}
-                              color={
-                                row.lastWebhookValidationStatus ===
-                                LastWebhookValidationStatusType.Active
-                                  ? 'success'
-                                  : 'error'
-                              }
-                            />
+                            >
+                              <Chip
+                                size={isMd ? 'small' : 'medium'}
+                                sx={{
+                                  ...(isMd && {
+                                    height: '15px',
+                                    fontSize: '1.5vw',
+                                  }),
+                                }}
+                                label={row.lastWebhookValidationStatus}
+                                color={
+                                  row.lastWebhookValidationStatus ===
+                                  LastWebhookValidationStatusType.Active
+                                    ? 'success'
+                                    : 'error'
+                                }
+                              />
+                              {row.importAttempts.find(
+                                (importAttempt) =>
+                                  importAttempt.status !==
+                                  ImportAttemptStatusType.InProgress,
+                              ) ? (
+                                <DeleteForeverIcon
+                                  sx={{
+                                    cursor: !isLoading ? 'pointer' : 'default',
+                                    ...(isMd && {
+                                      height: '15px',
+                                      fontSize: '3.5vw',
+                                    }),
+                                  }}
+                                  color={'error'}
+                                  onClick={() => handleDeleteToken(row.id)}
+                                />
+                              ) : null}
+                            </Box>
                           </TableCell>
                         </TableRow>
                       );
@@ -189,6 +261,24 @@ const ShowTokenList: React.FC = () => {
                   </TableBody>
                 </Table>
               </TableContainer>
+              {openAlertRemoveToken && (
+                <Dialog
+                  open={Boolean(openAlertRemoveToken)}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-delete-token"
+                  aria-describedby="alert-dialog-description--delete-token"
+                >
+                  <DialogTitle id="alert-dialog-title-delete-token">
+                    Ви впевнені, що хочете видалити токен?
+                  </DialogTitle>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Ні</Button>
+                    <Button onClick={handleConfirm} autoFocus>
+                      Так
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              )}
             </>
           ))}
       </>
