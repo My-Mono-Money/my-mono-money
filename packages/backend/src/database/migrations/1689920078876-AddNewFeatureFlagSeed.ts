76601eee42d926f52a3055d072b6f4976312d01d
Anton <example@gmail.com>
2023-07-21 09:18:18 +0300

    migration feature flag fix

--- packages/backend/src/database/migrations/1689920078876-AddNewFeatureFlagSeed.ts
+++ packages/backend/src/database/migrations/1689920078876-AddNewFeatureFlagSeed.ts
@@ -1,16 +1,16 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class FeatureFlagSeed1689859892844 implements MigrationInterface {
+export class AddNewFeatureFlagSeed1689920078876 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO "feature_flag" ("feature_name", "is_enabled")
-             VALUES ('bypass_monobank_rate_limit', true), ('verify_monobank_integration', true)`,
+                   VALUES ('verify_monobank_integration', false)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM "feature_flag" WHERE "feature_name" IN ('bypass_monobank_rate_limit', 'verify_monobank_integration')`,
+      `DELETE FROM "feature_flag" WHERE "feature_name" = 'verify_monobank_integration'`,
     );
   }
 }
